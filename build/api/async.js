38f79e47935e57caf313facc68508966927f922d
J Andrew Scott <jascott@programmer.net>
2021-07-16 16:03:21 -0400

    Nightly build

--- build/api/async.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncToPromise = void 0;
-function asyncToPromise(fn) {
-    return new Promise((resolve, reject) => {
-        fn((err, res) => err ? reject(err) : resolve(res));
-    });
-}
-exports.asyncToPromise = asyncToPromise;
